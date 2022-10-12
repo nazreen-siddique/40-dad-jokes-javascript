1a55ae76e8b48474fcfc9435d84bec43439a2c1c
Mohammad Asif Siddique <mdasifsidd@gmail.com>
2022-10-12 10:59:32 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+## Dad Jokes Project
+
+
+- [Dad Jokes](https://icanhazdadjoke.com/api)
+
+- random joke
+- https://icanhazdadjoke.com/
+
+
+
---
+++ app.js
@@ -0,0 +1,28 @@
+const url = 'https://icanhazdadjoke.com/';
+const btn = document.querySelector(".btn");
+const result = document.querySelector(".result");
+
+btn.addEventListener("click" ,()=>
+    fetchDadJoke(),
+);
+const fetchDadJoke = async () => {
+    result.textContent = "loading..."
+    try{const response = await fetch(url, {
+      headers: {
+        Accept: "application/json",
+        "user-Agent": "learning app",
+      },
+    });
+    if(!response.ok){
+        throw new Error("there is error")
+    }
+    const data = await response.json();
+    result.textContent = data.joke;}
+    catch(error){
+        console.log(error.message);
+    result.textContent = "sorry there was an error...";
+        
+    }
+       
+        };
+    fetchDadJoke();
